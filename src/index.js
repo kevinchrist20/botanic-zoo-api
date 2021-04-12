f7bc9bf38ee330700a01554cf636ca526cdb9b09
kevinchrist20 <kevinamevor@outlook.com>
2021-04-12 15:44:36 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var soup = require('jssoup')
+var axios = require('axios')
+
+
+// const animalUrl = 'https://animals.fandom.com/wiki/'
+const animalUrl = 'https://www.a-z-animals.com/animals/'
+const plantUrl = ''
+

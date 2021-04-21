95d11e54df6a385edcfd4616e8b8972619d57b59
kevinchrist20 <kevinamevor@outlook.com>
2021-04-21 13:11:57 +0000

    Implemented physical characteristics for animal

--- src/index.js
+++ src/index.js
@@ -9,18 +9,39 @@ axios
     .get(animalUrl + "lion")
     .then(response => {
         let $ = cheerio.load(response.data)
-        var factsBox = $('body').find('div.row.animal-facts-box')        
+        let factsBox = $('body').find('div.row.animal-facts-box')        
+        let animalDetails = $('body').find('div#single-animal-text')
+
         var classification = $(factsBox).find('dl.row.animal-facts')
         var locations = $(factsBox).children('.col-lg-6').next()
         var facts = $(factsBox).find('.col-lg-8 dl.row')
+        var physical = $(factsBox).find('.col-lg-4 dl.row')
+
+        $(animalDetails).each((i, ele) => {
+            console.log($(ele).text())
+        })
 
         // $(facts).each((i, ele) => {
         //     console.log($(ele).text())
         // })
 
-        $(facts).children().each((i, ele) => {
-            console.log($(ele).text())
-        })
+        // Facts
+        // $(facts).find('.col-md-6 .row').children().each((i, ele) => {
+        //     var key = $(ele).filter('dt').text()
+        //     var value = $(ele).filter('dd').text()
+        //     console.log(`${key}`)
+        // })
+
+        // Physical
+        // $(physical).children().each((i, ele) => {
+        //     var key = $(ele).filter('dt').text()
+        //     var value = $(ele).filter('dd')
+
+        //     if($(value).has('ul'))
+        //         console.log(`${key} ${value}`)
+        //     else 
+        //         console.log("NO")
+        // })
 
         // Locations
         // $(locations).find('ul li').each((i, ele) => {

594700c829259778747a580f5ae8d01213f5b0a1
kevinchrist20 <kevinamevor@outlook.com>
2021-04-22 09:25:04 +0000

    Scraped animal details

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ var styleConfig = {
 	listIndentionTabs: 2
 }
 
+var animal = {}
+
 axios
     .get(animalUrl + "lion")
     .then(response => {
@@ -27,43 +29,54 @@ axios
         var facts = $(factsBox).find('.col-lg-8 dl.row')
         var physical = $(factsBox).find('.col-lg-4 dl.row')
 
-
-        var animalDetailsHTML = animalDetails.html().replace(/(view all .* <a.*?>(.*?)<\/a>)/ig, "")
-        var detailsText = textVersion(animalDetailsHTML, styleConfig)
-        console.log(detailsText)
-
         // $(facts).each((i, ele) => {
         //     console.log($(ele).text())
         // })
 
         // Facts
-        // $(facts).find('.col-md-6 .row').children().each((i, ele) => {
-        //     var key = $(ele).filter('dt').text()
-        //     var value = $(ele).filter('dd').text()
-        //     console.log(`${key}`)
-        // })
+        $(facts).find('.col-md-6 .row').children().each((i, ele) => {
+            var key = $(ele).filter('dt').text()
+            var value = $(ele).filter('dd').text()
+
+            animal[key] = value
+        })
 
         // Physical
-        // $(physical).children().each((i, ele) => {
-        //     var key = $(ele).filter('dt').text()
-        //     var value = $(ele).filter('dd')
+        $(physical).children().each((i, ele) => {
+            var key = $(ele).filter('dt').text()
+            var value = $(ele).filter('dd')
 
-        //     if($(value).has('ul'))
-        //         console.log(`${key} ${value}`)
-        //     else 
-        //         console.log("NO")
-        // })
+            if($(value).has('ul')) {
+                animal[key] = value
+                console.log(`${key} ${value}`)
+            }
+            else 
+                animal[key] = value
+        })
 
         // Locations
-        // $(locations).find('ul li').each((i, ele) => {
-        //     console.log($(ele).text())
-        // })
+        $(locations).find('ul li').each((i, ele) => {
+            var location = $(ele).text()
+
+            animal['location'] = location
+        })
 
         // Classifications
-        // $(classification).children().each((i, ele) => {
-        //     var key = $(ele).filter('dt').text()
-        //     var value = $(ele).filter('dd').text()
-        //     console.log(`${key} ${value}`)
-        // })
+        $(classification).children().each((i, ele) => {
+            var key = $(ele).filter('dt').text()
+            var value = $(ele).filter('dd').text()
+
+            animal[key] = value
+            // console.log(`${key} ${value}`)
+        })
+
+        var animalDetailsHTML = animalDetails.html().replace(/(view all .* <a.*?>(.*?)<\/a>)/ig, "")
+        var detailsText = textVersion(animalDetailsHTML, styleConfig)
+        animal['description'] = detailsText
+
+        // console.log(detailsText)
 
     }).catch(err => console.log(err))
+
+
+    console.log(animal)
